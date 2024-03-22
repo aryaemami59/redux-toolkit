d3ca2dfa9d8e732a8ee3ac312ece2cd2eaa64a5e
Arya Emami <aryaemami59@yahoo.com>
2024-03-22 09:28:57 -0500

    Update IfUncheckedIndexedAccess type definition

--- packages/toolkit/src/uncheckedindexed.ts
+++ packages/toolkit/src/uncheckedindexed.ts
@@ -1,11 +1,11 @@
 // inlined from https://github.com/EskiMojo14/uncheckedindexed
 
-import type { IfMaybeUndefined } from './tsHelpers'
-
 // relies on remaining as a TS file, not .d.ts
+export type IfUndefined<P, True, False> = [undefined] extends [P] ? True : False
+
 const testAccess = ({} as Record<string, 0>)['a']
 
-export type IfUncheckedIndexedAccess<True, False> = IfMaybeUndefined<
+export type IfUncheckedIndexedAccess<True, False> = IfUndefined<
   typeof testAccess,
   True,
   False
