f152f47a9fbea5a67232cb42e08c5519ae7c1745
Arya Emami <aryaemami59@yahoo.com>
2024-03-22 15:20:53 -0500

    Remove commented out code in `cacheCollection.ts`

--- packages/toolkit/src/query/core/buildMiddleware/cacheCollection.ts
+++ packages/toolkit/src/query/core/buildMiddleware/cacheCollection.ts
@@ -22,23 +22,6 @@ function isObjectEmpty(obj: Record<any, any>) {
   return true
 }
 
-// declare module '../../endpointDefinitions' {
-//   interface QueryExtraOptions<
-//     TagTypes extends string,
-//     ResultType,
-//     QueryArg,
-//     BaseQuery extends BaseQueryFn,
-//     ReducerPath extends string = string,
-//   > {
-//     /**
-//      * Overrides the api-wide definition of `keepUnusedDataFor` for this endpoint only. _(This value is in seconds.)_
-//      *
-//      * This is how long RTK Query will keep your data cached for **after** the last component unsubscribes. For example, if you query an endpoint, then unmount the component, then mount another component that makes the same request within the given time frame, the most recent value will be served from the cache.
-//      */
-//     keepUnusedDataFor?: number
-//   }
-// }
-
 // Per https://developer.mozilla.org/en-US/docs/Web/API/setTimeout#maximum_delay_value , browsers store
 // `setTimeout()` timer values in a 32-bit int. If we pass a value in that's larger than that,
 // it wraps and ends up executing immediately.
