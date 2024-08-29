3c367357a73dfa1bcc7f0c72f55964ff30262d51
Arya Emami <aryaemami59@yahoo.com>
2024-08-29 08:33:03 -0500

    Add `knip`

---
+++ packages/toolkit/knip.config.ts
@@ -0,0 +1,34 @@
+import type { KnipConfig } from 'knip'
+
+const knipConfig = {
+  entry: [
+    'src/index.ts',
+    'src/react/index.ts',
+    'src/query/index.ts',
+    'src/query/react/index.ts',
+  ],
+  ignore: ['**/tests/**'],
+  typescript: { config: 'tsconfig.json' },
+  project: ['src/**', 'scripts/**'],
+  vitest: { config: 'vitest.config.mts' },
+  tsup: { config: 'tsup.config.ts' },
+  'size-limit': { config: '.size-limit.cjs' },
+  include: [
+    'dependencies',
+    'exports',
+    'files',
+    'devDependencies',
+    'optionalPeerDependencies',
+    'unlisted',
+    'binaries',
+    'unresolved',
+    'types',
+    'nsExports',
+    'nsTypes',
+    'duplicates',
+    'enumMembers',
+    'classMembers',
+  ],
+} satisfies KnipConfig
+
+export default knipConfig
