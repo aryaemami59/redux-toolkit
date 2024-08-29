bb590ab04dc0a156158a23fd83fc24f15533687d
Arya Emami <aryaemami59@yahoo.com>
2024-08-29 10:10:26 -0500

    Sort exports

--- packages/toolkit/src/query/index.ts
+++ packages/toolkit/src/query/index.ts
@@ -1,7 +1,13 @@
 // This must remain here so that the `mangleErrors.cjs` build script
 // does not have to import this into each source file it rewrites.
-import { formatProdErrorMessage } from '@reduxjs/toolkit'
 
+export type { Api, ApiContext, Module } from './apiTypes'
+export type {
+  BaseQueryApi,
+  BaseQueryEnhancer,
+  BaseQueryFn,
+} from './baseQueryTypes'
+export { QueryStatus } from './core/apiState'
 export type {
   CombinedState,
   QueryCacheKey,
@@ -10,65 +16,18 @@ export type {
   RootState,
   SubscriptionOptions,
 } from './core/apiState'
-export { QueryStatus } from './core/apiState'
-export type { Api, ApiContext, Module } from './apiTypes'
-
 export type {
-  BaseQueryApi,
-  BaseQueryArg,
-  BaseQueryEnhancer,
-  BaseQueryError,
-  BaseQueryExtraOptions,
-  BaseQueryFn,
-  BaseQueryMeta,
-  BaseQueryResult,
-  QueryReturnValue,
-} from './baseQueryTypes'
-export type {
-  BaseEndpointDefinition,
-  EndpointDefinitions,
-  EndpointDefinition,
-  EndpointBuilder,
-  QueryDefinition,
-  MutationDefinition,
-  MutationExtraOptions,
-  TagDescription,
-  QueryArgFrom,
-  QueryExtraOptions,
-  ResultTypeFrom,
-  DefinitionType,
-  DefinitionsFromApi,
-  OverrideResultType,
-  ResultDescription,
-  TagTypesFromApi,
-  UpdateDefinitions,
-} from './endpointDefinitions'
-export { fetchBaseQuery } from './fetchBaseQuery'
-export type {
-  FetchBaseQueryArgs,
-  FetchBaseQueryError,
-  FetchBaseQueryMeta,
-  FetchArgs,
-} from './fetchBaseQuery'
-export { retry } from './retry'
-export type { RetryOptions } from './retry'
-export { setupListeners } from './core/setupListeners'
-export { skipToken } from './core/buildSelectors'
-export type {
-  QueryResultSelectorResult,
-  MutationResultSelectorResult,
-  SkipToken,
-} from './core/buildSelectors'
-export type {
-  QueryActionCreatorResult,
   MutationActionCreatorResult,
+  QueryActionCreatorResult,
   StartQueryActionCreatorOptions,
 } from './core/buildInitiate'
-export type { CreateApi, CreateApiOptions } from './createApi'
-export { buildCreateApi } from './createApi'
-export { _NEVER, fakeBaseQuery } from './fakeBaseQuery'
-export { copyWithStructuralSharing } from './utils/copyWithStructuralSharing'
-export { createApi, coreModule, coreModuleName } from './core/index'
+export { skipToken } from './core/buildSelectors'
+export type {
+  MutationResultSelectorResult,
+  QueryResultSelectorResult,
+  SkipToken,
+} from './core/buildSelectors'
+export { coreModule, coreModuleName, createApi } from './core/index'
 export type {
   ApiEndpointMutation,
   ApiEndpointQuery,
@@ -76,11 +35,42 @@ export type {
   CoreModule,
   PrefetchOptions,
 } from './core/module'
+export { setupListeners } from './core/setupListeners'
+export { buildCreateApi } from './createApi'
+export type { CreateApi, CreateApiOptions } from './createApi'
 export { defaultSerializeQueryArgs } from './defaultSerializeQueryArgs'
 export type { SerializeQueryArgs } from './defaultSerializeQueryArgs'
-
+export type {
+  BaseEndpointDefinition,
+  DefinitionType,
+  DefinitionsFromApi,
+  EndpointBuilder,
+  EndpointDefinition,
+  EndpointDefinitions,
+  MutationDefinition,
+  MutationExtraOptions,
+  OverrideResultType,
+  QueryArgFrom,
+  QueryDefinition,
+  QueryExtraOptions,
+  ResultTypeFrom,
+  TagDescription,
+  TagTypesFromApi,
+  UpdateDefinitions,
+} from './endpointDefinitions'
+export { _NEVER, fakeBaseQuery } from './fakeBaseQuery'
+export { fetchBaseQuery } from './fetchBaseQuery'
+export type {
+  FetchArgs,
+  FetchBaseQueryArgs,
+  FetchBaseQueryError,
+  FetchBaseQueryMeta,
+} from './fetchBaseQuery'
+export { retry } from './retry'
+export type { RetryOptions } from './retry'
 export type {
   Id as TSHelpersId,
   NoInfer as TSHelpersNoInfer,
   Override as TSHelpersOverride,
 } from './tsHelpers'
+export { copyWithStructuralSharing } from './utils/copyWithStructuralSharing'
--- packages/toolkit/src/query/react/index.ts
+++ packages/toolkit/src/query/react/index.ts
@@ -1,33 +1,34 @@
 // This must remain here so that the `mangleErrors.cjs` build script
 // does not have to import this into each source file it rewrites.
-import { formatProdErrorMessage } from '@reduxjs/toolkit'
 
 import { buildCreateApi, coreModule } from '@reduxjs/toolkit/query'
-import { reactHooksModule, reactHooksModuleName } from './module'
+import { reactHooksModule } from './module'
 
-export * from '@reduxjs/toolkit/query'
-export { ApiProvider } from './ApiProvider'
-
-const createApi = /* @__PURE__ */ buildCreateApi(
+export const createApi = /* @__PURE__ */ buildCreateApi(
   coreModule(),
   reactHooksModule(),
 )
 
+export * from '@reduxjs/toolkit/query'
+export { ApiProvider } from './ApiProvider'
 export type {
-  TypedUseMutationResult,
-  TypedUseQueryHookResult,
-  TypedUseQueryStateResult,
-  TypedUseQuerySubscriptionResult,
   TypedLazyQueryTrigger,
-  TypedUseLazyQuery,
-  TypedUseMutation,
   TypedMutationTrigger,
   TypedQueryStateSelector,
-  TypedUseQueryState,
-  TypedUseQuery,
-  TypedUseQuerySubscription,
+  TypedUseLazyQuery,
   TypedUseLazyQuerySubscription,
+  TypedUseMutation,
+  TypedUseMutationResult,
+  TypedUseQuery,
+  TypedUseQueryHookResult,
+  TypedUseQueryState,
   TypedUseQueryStateOptions,
+  TypedUseQueryStateResult,
+  TypedUseQuerySubscription,
+  TypedUseQuerySubscriptionResult,
+  UseLazyQuery,
+  UseMutation,
+  UseQuery,
 } from './buildHooks'
 export { UNINITIALIZED_VALUE } from './constants'
-export { createApi, reactHooksModule, reactHooksModuleName }
+export { reactHooksModule, reactHooksModuleName } from './module'
