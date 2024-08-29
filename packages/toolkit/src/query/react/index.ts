e36bc947a93e1e144cc4774cb227097a753e84bd
Arya Emami <aryaemami59@yahoo.com>
2024-08-29 10:10:26 -0500

    Sort exports

--- packages/toolkit/src/query/react/index.ts
+++ packages/toolkit/src/query/react/index.ts
@@ -1,32 +1,30 @@
 // This must remain here so that the `mangleErrors.cjs` build script
 // does not have to import this into each source file it rewrites.
-import { formatProdErrorMessage } from '@reduxjs/toolkit'
 
 import { buildCreateApi, coreModule } from '@reduxjs/toolkit/query'
-import { reactHooksModule, reactHooksModuleName } from './module'
+import { reactHooksModule } from './module'
 
-export * from '@reduxjs/toolkit/query'
-export { ApiProvider } from './ApiProvider'
-
-const createApi = /* @__PURE__ */ buildCreateApi(
+export const createApi = /* @__PURE__ */ buildCreateApi(
   coreModule(),
   reactHooksModule(),
 )
 
+export * from '@reduxjs/toolkit/query'
+export { ApiProvider } from './ApiProvider'
 export type {
-  TypedUseMutationResult,
-  TypedUseQueryHookResult,
-  TypedUseQueryStateResult,
-  TypedUseQuerySubscriptionResult,
   TypedLazyQueryTrigger,
-  TypedUseLazyQuery,
-  TypedUseMutation,
   TypedMutationTrigger,
-  TypedUseQueryState,
-  TypedUseQuery,
-  TypedUseQuerySubscription,
+  TypedUseLazyQuery,
   TypedUseLazyQuerySubscription,
+  TypedUseMutation,
+  TypedUseMutationResult,
+  TypedUseQuery,
+  TypedUseQueryHookResult,
+  TypedUseQueryState,
+  TypedUseQueryStateResult,
+  TypedUseQuerySubscription,
+  TypedUseQuerySubscriptionResult,
   TypedUseQueryStateOptions,
 } from './buildHooks'
 export { UNINITIALIZED_VALUE } from './constants'
-export { createApi, reactHooksModule, reactHooksModuleName }
+export { reactHooksModule, reactHooksModuleName } from './module'
