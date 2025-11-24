bc20d76d97878adc960304ac7aae37b7a4b42178
Arya Emami <aryaemami59@yahoo.com>
2025-11-24 17:07:13 -0600

    minimize changes

--- packages/toolkit/src/reduxImports.ts
+++ packages/toolkit/src/reduxImports.ts
@@ -6,17 +6,3 @@ export {
   isAction,
   isPlainObject,
 } from 'redux'
-export type {
-  Action,
-  ActionCreator,
-  Dispatch,
-  Middleware,
-  MiddlewareAPI,
-  PreloadedStateShapeFromReducersMapObject,
-  Reducer,
-  ReducersMapObject,
-  StateFromReducersMapObject,
-  Store,
-  StoreEnhancer,
-  UnknownAction,
-} from 'redux'
