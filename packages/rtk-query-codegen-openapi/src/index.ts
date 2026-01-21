55f73c983e9795461d8e51087012937d98ac9afb
Arya Emami <aryaemami59@yahoo.com>
2026-01-21 07:58:07 -0600

    explicit types

--- packages/rtk-query-codegen-openapi/src/index.ts
+++ packages/rtk-query-codegen-openapi/src/index.ts
@@ -29,7 +29,7 @@ export async function generateEndpoints(options: GenerationOptions): Promise<str
   }
 }
 
-export function parseConfig(fullConfig: ConfigFile) {
+export function parseConfig(fullConfig: ConfigFile): (CommonOptions & OutputFileOptions)[] {
   const outFiles: (CommonOptions & OutputFileOptions)[] = [];
 
   if ('outputFiles' in fullConfig) {
@@ -44,7 +44,7 @@ export function parseConfig(fullConfig: ConfigFile) {
   } else {
     outFiles.push(fullConfig);
   }
-  return outFiles;
+  return outFiles satisfies (CommonOptions & OutputFileOptions)[];
 }
 
 /**
