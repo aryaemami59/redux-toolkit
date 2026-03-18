e2ec1ba80cc8812afa47cc0fb75e532b0eeb0552
Arya Emami <aryaemami59@yahoo.com>
2026-03-18 09:21:03 -0500

    resolve `tsdown` issues

--- packages/toolkit/src/entities/state_selectors.ts
+++ packages/toolkit/src/entities/state_selectors.ts
@@ -1,45 +1,59 @@
 import { createDraftSafeSelector } from '../createDraftSafeSelector'
-import type { CreateSelectorFunction, Selector } from '../reselectImports'
+import type { CreateSelectorFunction } from '../reselectImports'
 import type { EntityId, EntitySelectors, EntityState } from './models'
 
 type AnyCreateSelectorFunction = CreateSelectorFunction<any, any, any>
 
 export type GetSelectorsOptions = {
+  /**
+   * @default createDraftSafeSelector
+   */
   createSelector?: AnyCreateSelectorFunction
 }
 
-export function createSelectorsFactory<T, EntityIdType extends EntityId>() {
+export function createSelectorsFactory<
+  EntityType,
+  EntityIdType extends EntityId,
+>() {
   function getSelectors(
     selectState?: undefined,
     options?: GetSelectorsOptions,
-  ): EntitySelectors<T, EntityState<T, EntityIdType>, EntityIdType>
-  function getSelectors<V>(
-    selectState: (state: V) => EntityState<T, EntityIdType>,
+  ): EntitySelectors<
+    EntityType,
+    EntityState<EntityType, EntityIdType>,
+    EntityIdType
+  >
+  function getSelectors<StateType>(
+    selectState: (state: StateType) => EntityState<EntityType, EntityIdType>,
     options?: GetSelectorsOptions,
-  ): EntitySelectors<T, V, EntityIdType>
-  function getSelectors<V>(
-    selectState?: (state: V) => EntityState<T, EntityIdType>,
+  ): EntitySelectors<EntityType, StateType, EntityIdType>
+  function getSelectors<StateType>(
+    selectState?: (state: StateType) => EntityState<EntityType, EntityIdType>,
     options: GetSelectorsOptions = {},
-  ): EntitySelectors<T, any, EntityIdType> {
-    const {
-      createSelector = createDraftSafeSelector as AnyCreateSelectorFunction,
-    } = options
+  ): EntitySelectors<EntityType, any, EntityIdType> {
+    const { createSelector = createDraftSafeSelector } = options
 
-    const selectIds = (state: EntityState<T, EntityIdType>) => state.ids
+    const selectIds = (state: EntityState<EntityType, EntityIdType>) =>
+      state.ids
 
-    const selectEntities = (state: EntityState<T, EntityIdType>) =>
+    const selectEntities = (state: EntityState<EntityType, EntityIdType>) =>
       state.entities
 
     const selectAll = createSelector(
       selectIds,
       selectEntities,
-      (ids, entities): T[] => ids.map((id) => entities[id]),
+      (ids, entities): EntityType[] => ids.map((id) => entities[id]),
     )
 
-    const selectId = (_: unknown, id: EntityIdType) => id
+    const selectId = (
+      _state: EntityState<EntityType, EntityIdType>,
+      id: EntityIdType,
+    ) => id
 
-    const selectById = (entities: Record<EntityIdType, T>, id: EntityIdType) =>
-      entities[id]
+    const selectById = (
+      entities: Record<EntityIdType, EntityType>,
+      id: EntityIdType,
+    ) => entities[id]
 
     const selectTotal = createSelector(selectIds, (ids) => ids.length)
 
@@ -53,10 +67,7 @@ export function createSelectorsFactory<T, EntityIdType extends EntityId>() {
       }
     }
 
-    const selectGlobalizedEntities = createSelector(
-      selectState as Selector<V, EntityState<T, EntityIdType>>,
-      selectEntities,
-    )
+    const selectGlobalizedEntities = createSelector(selectState, selectEntities)
 
     return {
       selectIds: createSelector(selectState, selectIds),
