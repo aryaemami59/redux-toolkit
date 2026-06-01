bac51714c9984a3da9e23811d0e4786000a0c30b
Arya Emami <aryaemami59@yahoo.com>
2026-05-31 19:26:02 -0500

    update `vite` publish-ci example

--- examples/publish-ci/vite/src/main.tsx
+++ examples/publish-ci/vite/src/main.tsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { store } from './app/store'
 import App from './App'
+import { store } from './app/store'
 import './index.css'
-
 import { worker } from './mocks/browser'
 
 // Initialize the msw worker, wait for the service worker registration to resolve, then mount
 async function render() {
   await worker.start()
 
-  const rootNode = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement,
-  )
+  const rootNode = createRoot(document.getElementById('root') as HTMLElement)
 
   rootNode.render(
     <React.StrictMode>
